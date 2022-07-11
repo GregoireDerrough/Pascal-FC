b40415073d9100a5623b81e7e3cf313d9885fe73
GregoireDerrough <gregoirederrough@hotmail.co.uk>
2022-07-12 00:43:26 +0100

    feat: added batch file for documentation

---
+++ doc/create_documentation.bat
@@ -0,0 +1,3 @@
+REM TODO: need to improve this a bit, right now only works for windows
+gswin64c.exe -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=lrm.pdf lrm.ps
+gswin64c.exe -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=pc_ug.pdf pc_ug.ps
